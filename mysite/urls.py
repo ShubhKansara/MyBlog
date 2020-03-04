bced778f8e7e0eebce38d8fab8240d8d3ddfb95e
ShubhKansara <shubh.k.kansara@gmailom>
2020-03-05 00:02:05 +0530

    initial commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('myblog/', include('myblog.urls')),
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
