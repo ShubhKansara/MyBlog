96c2a37cf8f407262f6192ac02e0fa654106a38e
ShubhKansara <shubh.k.kansara@gmail.com>
2020-03-05 12:58:06 +0530

    New Modified

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
+app_name='article'
 urlpatterns = [
     path('', views.article_list, name='article-list'),
     re_path(r'^(?P<slug>[\w-]+)/', views.article_details, name='articles-details'),
